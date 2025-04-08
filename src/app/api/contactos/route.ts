558eea7842e018426dd8113ae24da896e2b9cd38
yonatanberruecos <yonatanberruecos.yb@gmail.com>
2025-04-07 23:34:37 -0500

    logica crear negocios y asociarlos a producto y a contacto

--- src/app/api/contactos/route.ts
+++ src/app/api/contactos/route.ts
@@ -11,13 +11,123 @@ export async function OPTIONS() {
     });
 }
 
-export async function POST(req: Request) {
-    try {
-        const body = await req.json();
+interface IdatosFormulario {
+    codigo_inmueble: string
+    firstname: string
+    lastname?: string
+    email:string
+    phone: string
+    mensaje_formulario: string
+}
 
-        // Token de autenticación (debería estar en variables de entorno)
-        const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
-        const HUBSPOT_ENDPOINT = "https://api.hubapi.com/crm/v3/objects/contacts";
+interface AsesorData {
+    rol_tr: string;
+    iduser_tr: string;
+    nrol: string;
+    ntercero: string;
+    celular: string;
+    fijo: string;
+    correo: string;
+    Direccion: string;
+    cedtercero: string;
+    tDoctercero: string;
+    vip: string;
+    FotoAsesor: string;
+    NomtDoctercero: string;
+}
+
+interface CaptadorData {
+    rol_tr: string; // Código del rol
+    iduser_tr: string; // ID del usuario
+    nrol: string; // Nombre del rol, ej: "Captador"
+    ntercero: string; // Nombre completo del tercero
+    celular: string;
+    fijo: string;
+    correo: string;
+    Direccion: string;
+    cedtercero: string; // Cédula o NIT
+    tDoctercero: string; // Tipo de documento como código
+    vip: 'Sí' | 'No';
+    FotoAsesor: string; // URL de la foto
+    NomtDoctercero: string; // Nombre del tipo de documento
+  }
+
+interface Inmueble {
+    idInm: string;
+    codinm: string;
+    IdInmobiliaria: string;
+    NombresGestion: string;
+    tpinmu: string;
+    NombreB: string;
+    fecha_modificacion: string;
+    alcobas: string;
+    banos: string;
+    garaje: string;
+    ValorVenta: string;
+    ValorCanon: string;
+    Direccion: string;
+    AreaConstruida: string;
+    AreaLote: string;
+    descripcionlarga :string;
+    latitud: string;
+    longitud: string;
+    nlocalidad: string;
+    NombreZ: string;
+    nciudad: string;
+    Estrato: string;
+    ValorIva: string;
+    Administracion: string;
+    DescDestinacion: string;
+    DescEstado: string;
+    EdadInmueble: string;
+    sede: string;
+    ndepto: string;
+    FConsignacion: string;
+    restricciones: string;
+    AdmonIncluida: string;
+    oper: string;
+    precio: string;
+    esNuevo: string;
+    depto: string;
+    Tipo_Inmueble: string;
+    ciudad: string;
+    Gestion: string;
+    zona: string;
+    localidad: string;
+    nombreDestinacion: string;
+    asesor: AsesorData[];
+    captador: CaptadorData[];
+    amobladoInmueble: boolean;
+    barrio: string;
+}
+
+const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY;
+const HUBSPOT_API = "https://api.hubapi.com";
+const SIMI_TOKEN = process.env.SIMI_TOKEN;
+
+const fetchHubSpot = async (url: string, method = 'GET', body?: any) => {
+    const options: RequestInit = {
+        method,
+        headers: {
+        Authorization: `Bearer ${HUBSPOT_API_KEY}`,
+        'Content-Type': 'application/json',
+        },
+        body: body ? JSON.stringify(body) : undefined,
+    };
+
+    const response = await fetch(`${HUBSPOT_API}${url}`, options);
+    if (!response.ok) {
+        const error = await response.json();
+        throw new Error(JSON.stringify(error));
+    }
+
+    return response.json();
+};
+
+export async function POST(req: Request) {
+    const urlSimi = 'http://simi-api.com/ApiSimiweb/response/v2/inmueble/codInmueble/';
+    try {
+        const body:IdatosFormulario  = await req.json();
 
         if (!HUBSPOT_API_KEY) {
             return NextResponse.json(
@@ -26,33 +136,232 @@ export async function POST(req: Request) {
             );
         }
 
-        // Formatear los datos para HubSpot
-        const hubspotData = {
-            properties: {
-                firstname: body.firstname,
-                email: body.email,
-                phone: body.phone,
-                mensaje_formulario: body.mensaje_formulario,
-            },
-        };
+        const {
+            codigo_inmueble,
+            firstname,
+            lastname,
+            mensaje_formulario,
+            phone,
+            email,
+        } = body;
 
-        // Enviar datos a HubSpot
-        const response = await fetch(HUBSPOT_ENDPOINT, {
-            method: "POST",
+        const responseSimi: any = await fetch(urlSimi + codigo_inmueble, {
+            method: 'GET',
             headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${HUBSPOT_API_KEY}`, // Autenticación Bearer
+                'Authorization': `Basic ${SIMI_TOKEN}`,
+                'Content-Type': 'application/json'
             },
-            body: JSON.stringify(hubspotData),
         });
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.message || "Error al enviar datos a HubSpot");
+        const product: Inmueble = await responseSimi.json();
+
+        const dataInmuebleCreate: any= { properties: {
+            name: `${product.Tipo_Inmueble}-${product.idInm}`,
+            hs_sku: product.idInm,
+            codigo_inmueble: product.idInm,
+            fingreso: product.FConsignacion, // Descripción del producto
+            alcobas: product.alcobas, // Precio del producto
+            banios: product.banos, // Tipo de producto (opcional)
+            garaje: product.garaje, // Código SKU del producto
+            estadoinmueble: product.DescEstado, // Precio en HubSpot
+            administracion: product.Administracion,
+            estrato: product.Estrato,
+            tipo_inmueble: product.Tipo_Inmueble,
+            gestion: product.Gestion,
+            price: parseFloat(product.ValorVenta.replace(/,/g, '')),
+            venta: product.ValorVenta,
+            canon: product.ValorCanon,
+            descripcionlarga: product.descripcionlarga,
+            areaconstruida: product.AreaConstruida,
+            arealote: product.AreaLote,
+            latitud: product.latitud,
+            longitud: product.longitud,
+            barrio: product.barrio,
+            ciudad: product.ciudad,
+            departamento: product.ndepto,
+            zona: product.zona,
+            sede: product.sede,
+            amobladoinmueble: product.amobladoInmueble,
+            admonincluida: product.AdmonIncluida
+        }}
+
+        const dataInmuebleUpdate: any= { properties: {
+            name: `${product.Tipo_Inmueble}-${product.idInm}`,
+            codigo_inmueble: product.idInm,
+            fingreso: product.FConsignacion, // Descripción del producto
+            alcobas: product.alcobas, // Precio del producto
+            banios: product.banos, // Tipo de producto (opcional)
+            garaje: product.garaje, // Código SKU del producto
+            estadoinmueble: product.DescEstado, // Precio en HubSpot
+            administracion: product.Administracion,
+            estrato: product.Estrato,
+            tipo_inmueble: product.Tipo_Inmueble,
+            gestion: product.Gestion,
+            price: parseFloat(product.ValorVenta.replace(/,/g, '')),
+            venta: product.ValorVenta,
+            canon: product.ValorCanon,
+            descripcionlarga: product.descripcionlarga,
+            areaconstruida: product.AreaConstruida,
+            arealote: product.AreaLote,
+            latitud: product.latitud,
+            longitud: product.longitud,
+            barrio: product.barrio,
+            ciudad: product.ciudad,
+            departamento: product.ndepto,
+            zona: product.zona,
+            sede: product.sede,
+            amobladoinmueble: product.amobladoInmueble,
+            admonincluida: product.AdmonIncluida
+        }}
+
+        const productSearch = await fetchHubSpot('/crm/v3/objects/products/search', 'POST', {
+            filterGroups: [{
+              filters: [{
+                propertyName: 'hs_sku',
+                operator: 'EQ',
+                value: codigo_inmueble,
+              }]
+            }],
+            properties: ['hs_object_id', 'name', 'hs_sku'],
+        });
+
+        let productId: string;
+
+        if (productSearch.total > 0) {
+            // 2. Update product if exists
+            productId = productSearch.results[0].id;
+            await fetchHubSpot(`/crm/v3/objects/products/${productId}`, 'PATCH', dataInmuebleUpdate);
+          } else {
+            // 3. Create product
+            const newProduct = await fetchHubSpot('/crm/v3/objects/products', 'POST', dataInmuebleCreate);
+            productId = newProduct.id;
         }
 
+        // 4. Find or create contact
+        let contactId: string;
+
+        const contactSearch = await fetchHubSpot('/crm/v3/objects/contacts/search', 'POST', {
+            filterGroups: [{
+                filters: [{
+                propertyName: 'email',
+                operator: 'EQ',
+                value: email,
+                }]
+            }]
+        });
+
+        if (contactSearch.total > 0) {
+            contactId = contactSearch.results[0].id;
+        } else {
+            const newContact = await fetchHubSpot('/crm/v3/objects/contacts', 'POST', {
+                properties: {
+                    firstname: firstname,
+                    email: email,
+                    phone: phone,
+                    mensaje_formulario: mensaje_formulario,
+                }
+        });
+            contactId = newContact.id;
+        }
+
+        const precio = product.ValorVenta == '0' ? parseFloat(product.ValorCanon.replace(/,/g, '')) : parseFloat(product.ValorVenta.replace(/,/g, ''));
+
+         // 5. Create deal
+        const newDeal = await fetchHubSpot('/crm/v3/objects/deals', 'POST', {
+            properties: {
+                dealname: `Interés en ${product.Tipo_Inmueble}-${product.idInm}`,
+                pipeline: 'default',
+                dealstage: 'appointmentscheduled',
+                amount: precio,
+            }
+        });
+
+        const dealId = newDeal.id;
+        
+        // Create the line item
+        const lineItemRes = await fetch(`https://api.hubapi.com/crm/v3/objects/line_items`, {
+            method: 'POST',
+            headers: {
+            'Authorization': `Bearer ${HUBSPOT_API_KEY}`,
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+            properties: {
+                name: `${product.Tipo_Inmueble}-${product.idInm}`,
+                quantity: 1,
+                price: precio, // example price
+            }
+            })
+        });
+  
+        const lineItemData = await lineItemRes.json();
+        const lineItemId = lineItemData.id;
+        console.log('lineItem', lineItemId);
+  
+        // 1. Associate Line Item with Product
+        await fetch(`https://api.hubapi.com/crm/v4/objects/line_items/${lineItemId}/associations/products/${productId}`, {
+            method: 'PUT',
+            headers: {
+            'Authorization': `Bearer ${HUBSPOT_API_KEY}`,
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                types: [
+                    {
+                    associationCategory: "HUBSPOT_DEFINED",
+                    associationTypeId: 20 // this is the correct association between line item and product
+                    }
+                ]
+            }),
+        });
+    
+        // 2. Associate Line Item with Deal
+        await fetch(`https://api.hubapi.com/crm/v4/objects/line_items/${lineItemId}/associations/deals/${dealId}`, {
+            method: 'PUT',
+            headers: {
+            'Authorization': `Bearer ${HUBSPOT_API_KEY}`,
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                types: [
+                    {
+                    associationCategory: "HUBSPOT_DEFINED",
+                    associationTypeId: 19 // association from line item to deal
+                    }
+                ]
+            }),
+        });
+  
+        // 7. Associate deal with contact
+        await fetchHubSpot(`/crm/v3/objects/deals/${dealId}/associations/contacts/${contactId}/3`, 'PUT');
+
+        // Formatear los datos para HubSpot
+        // const hubspotData = {
+        //     properties: {
+        //         firstname: body.firstname,
+        //         email: body.email,
+        //         phone: body.phone,
+        //         mensaje_formulario: body.mensaje_formulario,
+        //     },
+        // };
+
+        // Enviar datos a HubSpot
+        // const response = await fetch(HUBSPOT_ENDPOINT, {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //         Authorization: `Bearer ${HUBSPOT_API_KEY}`, // Autenticación Bearer
+        //     },
+        //     body: JSON.stringify(hubspotData),
+        // });
+
+        // if (!response.ok) {
+        //     const errorData = await response.json();
+        //     throw new Error(errorData.message || "Error al enviar datos a HubSpot");
+        // }
+
         return NextResponse.json(
-            { success: true, message: "Contact sent to HubSpot successfully" },
+            { success: true, message: "Contact sent to HubSpot successfully", product: dealId },
             {
               headers: new Headers({
                 "Access-Control-Allow-Origin": "*", // Allow all origins
--- src/app/api/productos/route.ts
+++ src/app/api/productos/route.ts
@@ -11,10 +11,11 @@ export async function OPTIONS() {
     });
 }
 
-interface IsimiInmueble {
+interface Inmueble {
     Codigo_Inmueble: string;
     IdInmobiliaria: string;
     fingreso: string;
+    fecha_modificacion: string;
     Alcobas: string;
     banios: string;
     garaje: string;
@@ -24,8 +25,6 @@ interface IsimiInmueble {
     Estrato: string;
     idTipoInmueble: string;
     Tipo_Inmueble: string;
-    idGestion: string;
-    Gestion: string;
     Venta: string;
     Canon: string;
     valorFiltro: string;
@@ -34,10 +33,12 @@ interface IsimiInmueble {
     AreaLote: string;
     latitud: string;
     longitud: string;
-    EdadInmueble: null;
+    EdadInmueble: number;
     NombreInmo: string;
     foto1: string;
     logo: string;
+    idGestion: string;
+    Gestion: string;
     Barrio: string;
     Ciudad: string;
     Departamento: string;
@@ -45,11 +46,29 @@ interface IsimiInmueble {
     foto360: number;
     sede: string;
     id_sede: string;
-    video360: null;
+    video360: null | string;
+    amobladoInmueble: boolean;
+    destacado: null | string;
+    amoblado: number;
+    inversion: null | string;
+    admonIncluida: string;
+}
+  
+interface DatosGenerales {
+    inicio: number;
+    fin: number;
+    pagina_actual: string;
+    totalInmuebles: number;
+    totalPagina: string;
+    valMaxVenta: string;
+    valMinVenta: string;
+    valMaxArriendo: string;
+    valMinArriendo: string;
 }
 
-interface IsimiResponse {
-    "0" : IsimiInmueble
+interface Isimidata {
+    Inmuebles: Inmueble[];
+    datosGrales: DatosGenerales;
 }
 
 export async function GET() {
@@ -58,7 +77,7 @@ export async function GET() {
         const simiToken = process.env.SIMI_TOKEN;
 
         const urlHubspot = 'https://api.hubapi.com/crm/v3/objects/products';
-        const urlSimi = 'http://www.simi-api.com/ApiSimiweb/response/v21/inmueblesDestacados/total/10/limite/1';
+        const urlSimi = 'https://simi-api.com/ApiSimiweb/response/v2.1.1/filtroInmueble/order/desc/limite/83/cantidad/1/total/10/order/desc/departamento/0/ciudad/0/zona/0/barrio/0/tipoInm/0/tipOper/0/areamin/0/areamax/0/valmin/0/valmax/0/banios/0/alcobas/0/garajes/0/sede/0/usuario/0';
 
         const responseSimi: any = await fetch(urlSimi, {
             method: 'GET',
@@ -70,59 +89,120 @@ export async function GET() {
 
         const dataSimi = await responseSimi.json();
 
-        if (!dataSimi["0"]) {
+        if (!dataSimi.Inmuebles[0]) {
             return Response.json({ error: "No se encontró el primer inmueble" }, { status: 404 });
         }
 
-        const firstProperty: IsimiInmueble = dataSimi["0"];
+        const products: Inmueble = dataSimi.Inmuebles[0];
         
         // Configurar las propiedades del producto en HubSpot
-        const productData = {
+        // const productData = {
+        //     properties: {
+        //         name: `${products.Tipo_Inmueble}-${products.Codigo_Inmueble}`,
+        //         hs_sku: products.Codigo_Inmueble,
+        //         codigo_inmueble: products.Codigo_Inmueble,
+        //         fingreso: products.fingreso, // Descripción del producto
+        //         alcobas: products.Alcobas, // Precio del producto
+        //         banios: products.banios, // Tipo de producto (opcional)
+        //         garaje: products.garaje, // Código SKU del producto
+        //         estadoinmueble: products.estadoInmueble, // Precio en HubSpot
+        //         administracion: products.Administracion,
+        //         estrato: products.Estrato,
+        //         tipo_inmueble: products.Tipo_Inmueble,
+        //         gestion: products.Gestion,
+        //         price: parseFloat(products.Venta.replace(/,/g, '')),
+        //         venta: products.Venta,
+        //         canon: products.Canon,
+        //         valorfiltro: products.valorFiltro,
+        //         descripcionlarga: products.descripcionlarga,
+        //         areaconstruida: products.AreaConstruida,
+        //         arealote: products.AreaLote,
+        //         latitud: products.latitud,
+        //         longitud: products.longitud,
+        //         foto1: products.foto1,
+        //         logo: products.logo,
+        //         barrio: products.Barrio,
+        //         ciudad: products.Ciudad,
+        //         departamento: products.Departamento,
+        //         zona: products.Zona,
+        //         foto360: products.foto360,
+        //         sede: products.sede,
+        //         amobladoinmueble: products.amobladoInmueble,
+        //         amoblado: products.amoblado,
+        //         inversion: products.inversion || '',
+        //         admonincluida: products.admonIncluida
+        //     }
+        // };
+
+        const productPayload = dataSimi.Inmuebles.map((product: Inmueble) => ({
             properties: {
-                name: `${firstProperty.Tipo_Inmueble}-${firstProperty.Codigo_Inmueble}`,
-                hs_sku: firstProperty.Codigo_Inmueble,
-                codigo_inmueble: firstProperty.Codigo_Inmueble,
-                fingreso: firstProperty.fingreso, // Descripción del producto
-                alcobas: firstProperty.Alcobas, // Precio del producto
-                banios: firstProperty.banios, // Tipo de producto (opcional)
-                garaje: firstProperty.garaje, // Código SKU del producto
-                estadoinmueble: firstProperty.estadoInmueble, // Precio en HubSpot
-                administracion: firstProperty.Administracion,
-                estrato: firstProperty.Estrato,
-                tipo_inmueble: firstProperty.Tipo_Inmueble,
-                gestion: firstProperty.Gestion,
-                venta: firstProperty.Venta,
-                canon: firstProperty.Canon,
-                valorfiltro: firstProperty.valorFiltro,
-                descripcionlarga: firstProperty.descripcionlarga,
-                areaconstruida: firstProperty.AreaConstruida,
-                arealote: firstProperty.AreaLote,
-                latitud: firstProperty.latitud,
-                longitud: firstProperty.longitud,
-                foto1: firstProperty.foto1,
-                logo: firstProperty.logo,
-                barrio: firstProperty.Barrio,
-                ciudad: firstProperty.Ciudad,
-                departamento: firstProperty.Departamento,
-                zona: firstProperty.Zona,
-                foto360: firstProperty.foto360,
-                sede: firstProperty.sede
-            }
-        };
-
-        // Realizar la solicitud a HubSpot
-        const response = await fetch(urlHubspot, {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${hubspotToken}`,
-                'Content-Type': 'application/json'
+                name: `${product.Tipo_Inmueble}-${product.Codigo_Inmueble}`,
+                hs_sku: product.Codigo_Inmueble,
+                codigo_inmueble: product.Codigo_Inmueble,
+                fingreso: product.fingreso, // Descripción del producto
+                alcobas: product.Alcobas, // Precio del producto
+                banios: product.banios, // Tipo de producto (opcional)
+                garaje: product.garaje, // Código SKU del producto
+                estadoinmueble: product.estadoInmueble, // Precio en HubSpot
+                administracion: product.Administracion,
+                estrato: product.Estrato,
+                tipo_inmueble: product.Tipo_Inmueble,
+                gestion: product.Gestion,
+                price: parseFloat(product.Venta.replace(/,/g, '')),
+                venta: product.Venta,
+                canon: product.Canon,
+                valorfiltro: product.valorFiltro,
+                descripcionlarga: product.descripcionlarga,
+                areaconstruida: product.AreaConstruida,
+                arealote: product.AreaLote,
+                latitud: product.latitud,
+                longitud: product.longitud,
+                foto1: product.foto1,
+                logo: product.logo,
+                barrio: product.Barrio,
+                ciudad: product.Ciudad,
+                departamento: product.Departamento,
+                zona: product.Zona,
+                foto360: product.foto360,
+                sede: product.sede,
+                amobladoinmueble: product.amobladoInmueble,
+                amoblado: product.amoblado,
+                inversion: product.inversion || '',
+                admonincluida: product.admonIncluida
             },
-            body: JSON.stringify(productData)
-        });
+          }));
 
-        const data = await response.json();
+          
 
-        return Response.json(data, { status: response.status });
+        // // Realizar la solicitud a HubSpot
+        // const response = await fetch(urlHubspot, {
+        //     method: 'POST',
+        //     headers: {
+        //         'Authorization': `Bearer ${hubspotToken}`,
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(productData)
+        // });
+
+        // const data = await response.json();
+
+        // return Response.json(data, { status: response.status });
+
+        // Send products in batches
+    const responses = await Promise.all(
+        productPayload.map(async (chunk: any) => {
+          return fetch(urlHubspot, {
+                method: 'POST',
+                headers: {
+                     'Authorization': `Bearer ${hubspotToken}`,
+                     'Content-Type': 'application/json'
+                 },
+                 body: JSON.stringify(chunk)
+            });
+        })
+      );
+  
+      return Response.json({ message: "Products added successfully", data: responses.map((r: any) => r.data) });
     } catch (error) {
         console.error("Error creando el producto en HubSpot:", error);
         return Response.json({ error: "Error creando el producto" }, { status: 500 });
